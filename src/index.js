9ed8ff11147fe71f9112864a5e6ed228b28f6086
deepak gupta <dg91918@gmail.com>
2025-05-18 21:04:47 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ const express = require("express");
 const messageRouter = require("./routers/messageRouter");
 const whatsappclient = require("./services/WhatsappClient");
 const cors = require("cors");
-
+app.use(cors());
 whatsappclient.initialize().catch((err) => console.log(err));
 
 const app = express();
 app.use(express.json());
 app.use(messageRouter);
-
-app.use(cors());
 
 app.listen(7000, () => console.log(`Server is ready in on port 7000`));
--- src/services/WhatsappClient.js
+++ src/services/WhatsappClient.js
@@ -50,14 +50,6 @@ function sendMessage(phoneNumber, message, clientId, file) {
   }
 }
 
-// const whatsappclient = new Client({
-//   webVersionCache: {
-//     type: "remote",
-//     remotePath:
-//       "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
-//   },
-// });
-
 const whatsappclient = new Client({
   webVersionCache: {
     type: "remote",
